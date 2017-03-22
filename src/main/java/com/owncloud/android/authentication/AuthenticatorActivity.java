a5eacc1ba0603d94ed045ab89ba09d532821109b
tobiasKaminsky <tobias@kaminsky.me>
2017-03-22 20:23:22 +0100

    Revert "remove firebase from branding branch and put it into branch "analytics""
    This reverts commit 71ea26df3186e0c76f30c81c388fa077affc844d.

--- src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -716,6 +716,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
     protected void onResume() {
         super.onResume();
 
+        MainApp.getFirebaseAnalyticsInstance().setCurrentScreen(this, SCREEN_NAME, TAG);
+
         // bound here to avoid spurious changes triggered by Android on device rotations
         mHostUrlInput.setOnFocusChangeListener(this);
         mHostUrlInput.addTextChangedListener(mHostUrlInputWatcher);
